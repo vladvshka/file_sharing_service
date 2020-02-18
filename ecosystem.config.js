64a249c46b3c9a117534bcde4683179979ea3149
Vladislav <brinvlad@mail.ru>
2020-02-18 22:24:19 +0300

    + pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	apps: [
+		{
+			name: 'File Uploader',
+			script: 'preload.js',
+
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				PORT: 8000,
+				NODE_ENV: 'development'
+			},
+			env_production: {
+				PORT: 8000,
+				NODE_ENV: 'production'
+			}
+		}
+	],
+
+	deploy: {
+		production: {
+			user: 'user',
+			host: 'localhost',
+			ref: 'origin/master',
+			repo: 'git@github.com:vladvshka/file_sharing_service.git',
+			path: '/deploy',
+			// TODO: add script restoring DB
+			// "pre-setup" : "apt-get install git",
+			'post-deploy':
+				'npm install && pm2 reload ecosystem.config.js --env production'
+		}
+	}
+};
